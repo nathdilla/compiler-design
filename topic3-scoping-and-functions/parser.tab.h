3594e58e0150af8d8d45f48382f2e271c9cfc5bb
EliStGCU <EStreitmat@my.gcu.edu>
2024-10-21 21:38:02 -0700

    Minor change - extern for live_defs

--- topic3-scoping-and-functions/parser.tab.h
+++ topic3-scoping-and-functions/parser.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.8.2.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,70 +31,75 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_PARSER_TAB_H_INCLUDED
+# define YY_YY_PARSER_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 1
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     TYPE = 258,
-     ID = 259,
-     SEMICOLON = 260,
-     LBRACK = 261,
-     RBRACK = 262,
-     LPAREN = 263,
-     RPAREN = 264,
-     COMMA = 265,
-     EQ = 266,
-     PLUS = 267,
-     MINUS = 268,
-     STAR = 269,
-     BSLASH = 270,
-     NUMBER = 271,
-     WRITE = 272,
-     RETURN = 273,
-     FUNC = 274
-   };
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    TYPE = 258,                    /* TYPE  */
+    ID = 259,                      /* ID  */
+    SEMICOLON = 260,               /* SEMICOLON  */
+    LBRACK = 261,                  /* LBRACK  */
+    RBRACK = 262,                  /* RBRACK  */
+    LPAREN = 263,                  /* LPAREN  */
+    RPAREN = 264,                  /* RPAREN  */
+    COMMA = 265,                   /* COMMA  */
+    EQ = 266,                      /* EQ  */
+    PLUS = 267,                    /* PLUS  */
+    MINUS = 268,                   /* MINUS  */
+    STAR = 269,                    /* STAR  */
+    BSLASH = 270,                  /* BSLASH  */
+    NUMBER = 271,                  /* NUMBER  */
+    WRITE = 272,                   /* WRITE  */
+    RETURN = 273,                  /* RETURN  */
+    FUNC = 274                     /* FUNC  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
-/* Tokens.  */
-#define TYPE 258
-#define ID 259
-#define SEMICOLON 260
-#define LBRACK 261
-#define RBRACK 262
-#define LPAREN 263
-#define RPAREN 264
-#define COMMA 265
-#define EQ 266
-#define PLUS 267
-#define MINUS 268
-#define STAR 269
-#define BSLASH 270
-#define NUMBER 271
-#define WRITE 272
-#define RETURN 273
-#define FUNC 274
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 41 "parser.y"
+union YYSTYPE
 {
+#line 41 "parser.y"
+
 	int number;
 	char character;
 	char* string;
 	char* operator;
 	struct ASTNode* ast;
-}
-/* Line 1529 of yacc.c.  */
-#line 95 "parser.tab.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 91 "parser.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_PARSER_TAB_H_INCLUDED  */
